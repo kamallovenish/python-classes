c6bd396225edd092601970a10208fd6197d5d359
kamallovenish <lovenishn@gmail.com>
2021-04-03 15:17:00 +0530

    Add files via upload

---
+++ lecture1.py
@@ -0,0 +1,505 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name = 'my name is python'
+
+
+# In[2]:
+
+
+name
+
+
+# In[3]:
+
+
+name2 = "python"
+
+
+# In[4]:
+
+
+name2
+
+
+# In[5]:
+
+
+a = 'hello world'
+
+
+# In[6]:
+
+
+a
+
+
+# In[7]:
+
+
+print ("hello world")
+      
+
+
+# In[8]:
+
+
+b =56
+
+
+# In[9]:
+
+
+b
+
+
+# In[10]:
+
+
+b =56.000
+
+
+# In[11]:
+
+
+b
+
+
+# In[12]:
+
+
+type (name2)
+
+
+# In[16]:
+
+
+type(b)
+
+
+# In[14]:
+
+
+b =6
+
+
+# In[15]:
+
+
+b
+
+
+# In[17]:
+
+
+type(b)
+
+
+# In[18]:
+
+
+c = 56.00
+
+
+# In[19]:
+
+
+c
+
+
+# In[20]:
+
+
+type (c)
+
+
+# In[21]:
+
+
+int (11.99)
+
+
+# In[22]:
+
+
+txt = '10'
+
+
+# In[23]:
+
+
+txt
+
+
+# In[24]:
+
+
+type (txt)
+
+
+# In[25]:
+
+
+'10+10'
+
+
+# In[26]:
+
+
+int ('10')
+
+
+# In[27]:
+
+
+int(txt
+   )
+
+
+# In[28]:
+
+
+num1 = int(txt)
+
+
+# In[29]:
+
+
+num1
+
+
+# In[30]:
+
+
+type (num1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[31]:
+
+
+num2 = float (num1)
+
+
+# In[32]:
+
+
+num2
+
+
+# In[33]:
+
+
+int ('python')
+
+
+# In[34]:
+
+
+str ('python')
+
+
+# In[35]:
+
+
+float (10.56)
+
+
+# In[ ]:
+
+
+
+
+
+# In[36]:
+
+
+float ("10.56")
+
+
+# In[37]:
+
+
+num4 = 10.56
+
+
+# In[38]:
+
+
+num4
+
+
+# In[39]:
+
+
+float (num4)
+
+
+# In[40]:
+
+
+int (num4
+    )
+
+
+# In[41]:
+
+
+int('50.23')
+
+
+# In[42]:
+
+
+float ('50.23')
+
+
+# In[43]:
+
+
+10==12
+
+
+# In[44]:
+
+
+12==12
+
+
+# In[45]:
+
+
+Python == python
+
+
+# In[46]:
+
+
+python == "Python"
+
+
+# In[47]:
+
+
+python == 'python'
+
+
+# In[48]:
+
+
+'python' == 'python'
+
+
+# In[49]:
+
+
+'Python' == 'python'
+
+
+# In[50]:
+
+
+str1 = 'life line'
+
+
+# In[51]:
+
+
+str1
+
+
+# In[52]:
+
+
+str2 == 'life line'
+
+
+# In[53]:
+
+
+a == 10
+
+
+# In[54]:
+
+
+abcd == 100
+
+
+# In[55]:
+
+
+str1
+
+
+# In[56]:
+
+
+str1 = 'life line'
+
+
+# In[57]:
+
+
+str1
+
+
+# In[58]:
+
+
+str1 = "'life 
+line'"
+
+
+# In[59]:
+
+
+str1 = "life 
+line"
+
+
+# In[60]:
+
+
+str1 = 'life \n line'
+
+
+# In[61]:
+
+
+str1
+
+
+# In[62]:
+
+
+'python + ml'
+
+
+# In[63]:
+
+
+"python ml"
+
+
+# In[64]:
+
+
+'python'*10
+
+
+# In[65]:
+
+
+'python'*100
+
+
+# In[66]:
+
+
+10+6
+
+
+# In[67]:
+
+
+56/8
+
+
+# In[68]:
+
+
+int ('5') + int ('6')
+
+
+# In[69]:
+
+
+'5'+ '6'
+
+
+# In[70]:
+
+
+text2 = "Hello's there"
+
+
+# In[71]:
+
+
+text2 
+
+
+# In[74]:
+
+
+text2='Hello \s There'
+
+
+# In[75]:
+
+
+text2
+
+
+# In[ ]:
+
+
+text2 = "hello\"s there"
+
+
+# In[ ]:
+
+
+text2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+text2
+
+
+# In[ ]:
+
+
+print (' \\ ')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+print (' // ')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
