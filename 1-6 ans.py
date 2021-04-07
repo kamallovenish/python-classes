a57764dad33177c3dbd0217920891aa4f7404852
kamallovenish <lovenishn@gmail.com>
2021-04-07 08:25:27 +0530

    Add files via upload

---
+++ 1-6 ans.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1) what is the difference between operators and value 
+
+
+# In[2]:
+
+
+#2) what is the difference between the string and variable ?
+
+
+# In[3]:
+
+
+#ans: A Variable is a store of information, and a String is a type of information for example 
+
+
+# In[4]:
+
+
+string = 'i love python' ---this is the string. 
+
+
+# In[5]:
+
+
+x= 'i love python'
+
+
+# In[6]:
+
+
+print (x)
+
+
+# In[7]:
+
+
+# 3) describe the three different data forms 
+
+
+# In[8]:
+
+
+# 1) int 2) flot 3) complex 
+
+
+# In[9]:
+
+
+a = int (5)
+
+
+# In[10]:
+
+
+type (a)
+
+
+# In[11]:
+
+
+a=5 
+
+
+# In[12]:
+
+
+type (a)
+
+
+# In[13]:
+
+
+print (a)
+
+
+# In[14]:
+
+
+b=5.0
+
+
+# In[15]:
+
+
+type (b)
+
+
+# In[16]:
+
+
+print (b)
+
+
+# In[17]:
+
+
+c= 5+1j
+
+
+# In[18]:
+
+
+type (c)
+
+
+# In[19]:
+
+
+print (c)
+
+
+# In[20]:
+
+
+#4) what should the value 
+
+
+# In[21]:
+
+
+'spam'+'spamspam'
+
+
+# In[22]:
+
+
+'spam'*3
+
+
+# In[23]:
+
+
+'spam',+'spamspam'
+
+
+# In[24]:
+
+
+#5 why is it that data1 is a true variable name but 100 is not 
+
+
+# In[25]:
+
+
+#Variable name should not be a Python keyword.Keywords are also called as reserved words.variable is always a string not numeric value. 
+
+
+# In[26]:
+
+
+# 
+
+
+# In[33]:
+
+
+stringfirst='Hello world'
+
+
+# In[37]:
+
+
+stringsecond='again!'
+
+
+# In[34]:
+
+
+stringfirst[3]
+
+
+# In[35]:
+
+
+stringfirst[0]
+
+
+# In[38]:
+
+
+stringsecond[2]
+
+
+# In[39]:
+
+
+stringfirst[-1]
+
+
+# In[40]:
+
+
+x=23
+
+
+# In[41]:
+
+
+y=20
+
+
+# In[42]:
+
+
+z=x-y
+
+
+# In[43]:
+
+
+print(z)
+
+
+# In[ ]:
+
+
+
+
