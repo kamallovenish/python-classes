bd33e47422f8f94dc79a641a8f79338329abadea
kamallovenish <lovenishn@gmail.com>
2021-04-10 01:55:35 +0530

    Add files via upload

---
+++ 8-20.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#(8
+
+
+# In[2]:
+
+
+a = True 
+
+
+# In[3]:
+
+
+type (a)
+
+
+# In[4]:
+
+
+b = False
+
+
+# In[5]:
+
+
+print (b)
+
+
+# In[6]:
+
+
+type (b)
+
+
+# In[7]:
+
+
+#(9
+
+
+# In[8]:
+
+
+int a = 10 
+
+
+# In[9]:
+
+
+a = 10 
+
+
+# In[10]:
+
+
+print (a)
+
+
+# In[11]:
+
+
+5==5
+
+
+# In[12]:
+
+
+print 7/5
+
+
+# In[14]:
+
+
+print -7 / 5
+
+
+# In[15]:
+
+
+-7 / 5
+
+
+# In[16]:
+
+
+7 / 5
+
+
+# In[17]:
+
+
+#Output in Python 2.x
+ 
+1
+ 
+-2
+ 
+
+
+# In[20]:
+
+
+print ('I have eaten  +5+  pizzas')
+
+
+# In[21]:
+
+
+#(11
+
+
+# In[22]:
+
+
+text2 = "hello\'s there"
+
+
+# In[23]:
+
+
+text2
+
+
+# In[24]:
+
+
+(7+8J)
+
+
+# In[25]:
+
+
+a = 7+8J
+
+
+# In[26]:
+
+
+type (a)
+
+
+# In[27]:
+
+
+s = ('I love python')
+
+
+# In[28]:
+
+
+print (s)
+
+
+# In[30]:
+
+
+print (len (s))
+
+
+# In[31]:
+
+
+print (s [4])
+
+
+# In[32]:
+
+
+print (s [-6])
+
+
+# In[33]:
+
+
+"int ("10.9")"
+
+
+# In[34]:
+
+
+a,b,c = 1,2,"john"
+
+
+# In[35]:
+
+
+print (c)
+
+
+# In[36]:
+
+
+print (a)
+
+
+# In[37]:
+
+
+print (b)
+
+
+# In[39]:
+
+
+string2 = "I love python"
+
+
+# In[40]:
+
+
+print (string2.split())
+
+
+# In[41]:
+
+
+print (string2.split(':'))
+
+
+# In[42]:
+
+
+#17
+
+
+# In[43]:
+
+
+-14//3
+
+
+# In[44]:
+
+
+-14//2
+
+
+# In[45]:
+
+
+14//2
+
+
+# In[47]:
+
+
+my_list1 = [1,2,3,4,5,6,7,8,9]
+
+
+# In[50]:
+
+
+del my_list1[2]
+
+
+# In[51]:
+
+
+print(my_list1)
+
+
+# In[52]:
+
+
+txt = "We are the so-called \"Vikings\" from the north."
+
+
+# In[53]:
+
+
+print (txt)
+
+
+# In[54]:
+
+
+txt = ",,,,,rrttgg.....banana....rrr"
+
+
+# In[55]:
+
+
+x = txt.strip(",.grt")
+
+
+# In[56]:
+
+
+print (x)
+
+
+# In[ ]:
+
+
+
+
