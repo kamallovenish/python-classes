dec0a605858f58c4d715238bb12d6a984567c5e9
kamallovenish <lovenishn@gmail.com>
2021-04-24 21:16:13 +0530

    Add files via upload

---
+++ lecture2.1.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#lecture 2.1  split 
+
+
+# In[3]:
+
+
+string2 = "We are learning python"
+
+
+# In[5]:
+
+
+print (string2.split(' '))
+
+
+# In[7]:
+
+
+print (string2.split[1])
+
+
+# In[8]:
+
+
+a =5 
+b = 7 
+c = 9
+
+
+# In[9]:
+
+
+print (a)
+
+
+# In[10]:
+
+
+a = b = c =5 
+
+
+# In[11]:
+
+
+print (b)
+
+
+# In[12]:
+
+
+a,b,c = 5,10,15
+
+
+# In[13]:
+
+
+print (c)
+
+
+# In[14]:
+
+
+print (a)
+
+
+# In[15]:
+
+
+print (b)
+
+
+# In[16]:
+
+
+num1 = 5
+
+
+# In[17]:
+
+
+num1 = num1+2 
+
+
+# In[18]:
+
+
+num1
+
+
+# In[19]:
+
+
+num1  = num1 + 3
+
+
+# In[20]:
+
+
+num1
+
+
+# In[21]:
+
+
+num1+=5
+
+
+# In[22]:
+
+
+num1
+
+
+# In[23]:
+
+
+num1 
+
+
+# In[25]:
+
+
+num1 *= 2
+
+
+# In[26]:
+
+
+num1
+
+
+# In[27]:
+
+
+5 == 5
+
+
+# In[28]:
+
+
+5 is 5 
+
+
+# In[29]:
+
+
+5 is 10 
+
+
+# In[32]:
+
+
+5 is not 10
+
+
+# 5 != 10
+
+# In[33]:
+
+
+5 != 10
+
+
+# In[34]:
+
+
+10 == 20
+
+
+# In[35]:
+
+
+10 != 20
+
+
+# In[36]:
+
+
+true =1 
+false =0 
+
+
+# In[37]:
+
+
+true + true
+
+
+# In[38]:
+
+
+False + False
+
+
+# In[39]:
+
+
+True + False
+
+
+# In[40]:
+
+
+5>=3
+
+
+# In[41]:
+
+
+5>3
+
+
+# In[42]:
+
+
+3>3
+
+
+# In[43]:
+
+
+3>=3
+
+
+# In[ ]:
+
+
+
+
