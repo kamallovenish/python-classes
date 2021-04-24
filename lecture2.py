559c25505785c4b081902742b13317252deb696a
kamallovenish <lovenishn@gmail.com>
2021-04-24 20:57:22 +0530

    Add files via upload

---
+++ lecture2.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# lecture 2 
+
+
+# In[2]:
+
+
+"30+20"
+
+
+# In[4]:
+
+
+#string,float and int 
+
+
+# In[5]:
+
+
+var1 = "My name is lovenish"
+
+
+# In[6]:
+
+
+print (var1)
+
+
+# In[7]:
+
+
+type (var1)
+
+
+# In[9]:
+
+
+"Python" == 'python'
+
+
+# In[10]:
+
+
+"Neeraj" == 'Neeraj'
+
+
+# In[11]:
+
+
+#\t -tab and \n - New line --Escape characters 
+
+
+# In[12]:
+
+
+#notation  - complex  -j 
+#complex number is all about an imaginary number 
+#i, j  both are - in python only J 
+
+
+# In[13]:
+
+
+a = 7+8j 
+
+
+# In[14]:
+
+
+type(a)
+
+
+# In[15]:
+
+
+print ('hello')
+
+
+# In[16]:
+
+
+print "hello"
+
+
+# In[17]:
+
+
+#python 2 and 3 
+
+
+# In[18]:
+
+
+5/3
+
+
+# In[19]:
+
+
+int (5/3)
+
+
+# In[24]:
+
+
+var1 = "My name is lovenish"
+
+
+# In[25]:
+
+
+print (len(var1))
+
+
+# In[26]:
+
+
+print (len(' '))
+
+
+# In[27]:
+
+
+print (len('dhhdhdhdhdhhdhdhd'))
+
+
+# In[28]:
+
+
+print (len(333333))
+
+
+# In[29]:
+
+
+print ('python' + 'love')
+
+
+# In[34]:
+
+
+print ('python' +  ' love')
+
+
+# In[36]:
+
+
+print ('python' + ' ' + 'love')
+
+
+# In[37]:
+
+
+string2 = "we are learning python"
+
+
+# In[38]:
+
+
+print (string2 [2])
+
+
+# In[39]:
+
+
+print (string2 [3])
+
+
+# In[40]:
+
+
+print (string2 [0])
+
+
+# In[41]:
+
+
+print (string2 [2*2])
+
+
+# In[42]:
+
+
+print (string2 [2/2])
+
+
+# In[43]:
+
+
+print (string2 [2+2])
+
+
+# In[44]:
+
+
+print (string2 [2-2])
+
+
+# In[45]:
+
+
+'t' + 'o'
+
+
+# In[46]:
+
+
+'t'*2
+
+
+# In[47]:
+
+
+print (string2 [-3])
+
+
+# In[48]:
+
+
+print (string2 [-1])
+
+
+# In[1]:
+
+
+name = "sudha"
+class1 = 12 
+subjects = "PCM"
+
+
+# In[2]:
+
+
+print (name + 'is in' + Class1 + 'with subjects' + subjects)
+
+
+# In[8]:
+
+
+print (name,'is in',class1,'with subjects',subjects)
+
+
+# In[ ]:
+
+
+name = input('Enter your name')
+class1 = input ('enter your classs')
+subjects = input ('Enter your subjects')
+print (name,'is in',class1,'with subjects',subjects)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
